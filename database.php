0c08364b3b4907e7fb1595fa2717b4119734a1b2
bernardop-dev <bdutra721@gmail.com>
2025-06-21 21:06:58 -0300

    Delete database.php

--- database.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db = new PDO('sqlite:tarefas.db');
-$db->exec("CREATE TABLE IF NOT EXISTS tarefas (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    descricao TEXT,
-    feita INTEGER DEFAULT 0
-)");
-?>
